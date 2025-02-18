e008653b4f6a3cddf6fba5344ab6f818b516053b
Da-StackOverflow <739050313@qq.com>
2025-02-18 12:17:39 +0800

    1.0

--- CopyFile/Program.cs
+++ CopyFile/Program.cs
@@ -117,17 +117,6 @@ var path = Path.GetFullPath("CopyFile.exe").Split("bin")[0];
 //}
 
 
-//try
-//{
-//	File.Copy("F:/CSharp/GTA5Trainer/GTA5Trainer/bin/GTA5Trainer.asi", "D:/SteamLibrary/steamapps/common/Grand Theft Auto V/GTA5Trainer.asi", true);
-//	Console.WriteLine("成功");
-//}
-//catch (Exception e)
-//{
-//	Console.WriteLine(e.Message);
-//	Console.WriteLine(e.StackTrace);
-//}
-
 bool gameRunning = false;
 
 if (!Directory.Exists("D:/SteamLibrary/steamapps/common/Grand Theft Auto V/GTA5Trainer"))
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 中文：
+
 在.Net Framework 4.8.1 框架下用C#方式实现了GTA5的内置修改器
 
 English:
+
 You Can Use C# to write your own GTA5 Trainer by using .Net Framework 4.8.1
