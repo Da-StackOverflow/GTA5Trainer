4379c9cd1c9c6e4510b9ddbfc130a31676d29e69
Da-StackOverflow <739050313@qq.com>
2025-02-19 17:01:55 +0800

    Update CopyTool

---
+++ CopyTool/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+
+namespace CopyTool
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string GameDirectory = "D:/SteamLibrary/steamapps/common/Grand Theft Auto V";
+
+			string GameScriptDirectory = $"{GameDirectory}/GTA5Trainer";
+
+			string ProjectBasePath = "../..";
+
+			if (!Directory.Exists(GameScriptDirectory))
+			{
+				Directory.CreateDirectory(GameScriptDirectory);
+			}
+
+			bool gameRunning = false;
+
+			if (!gameRunning)
+			{
+				try
+				{
+					File.Copy($"{ProjectBasePath}/Asi/bin/GTA5Trainer.asi", $"{GameDirectory}/GTA5Trainer.asi", true);
+					Console.WriteLine("Copy GTA5Trainer.asi Success");
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+					Console.WriteLine(e.StackTrace);
+				}
+
+
+				try
+				{
+					File.Copy($"{ProjectBasePath}/Bridge/bin/GTA5TrainerBridge.dll", $"{GameDirectory}/GTA5TrainerBridge.dll", true);
+					Console.WriteLine("Copy GTA5TrainerBridge.dll Success");
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+					Console.WriteLine(e.StackTrace);
+				}
+			}
+
+
+			try
+			{
+				File.Copy($"{ProjectBasePath}/Bridge/bin/GTA5TrainerBridge.dll", $"{GameDirectory}/GTA5Trainer/GTA5TrainerBridge.dll", true);
+				Console.WriteLine("Copy GTA5Trainer/GTA5TrainerBridge.dll Success");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+				Console.WriteLine(e.StackTrace);
+			}
+
+			try
+			{
+				File.Copy($"{ProjectBasePath}/ScriptUI/bin/GTA5TrainerScriptUI.dll", $"{GameDirectory}/GTA5Trainer/GTA5TrainerScriptUI.dll", true);
+				Console.WriteLine("Copy GTA5Trainer/GTA5TrainerScriptUI.dll Success");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+				Console.WriteLine(e.StackTrace);
+			}
+
+			string[] scripts = [
+				"Player",
+				"Vehicle",
+				"Weapon",
+				"Time",
+				"Weather",
+				"Other",
+			];
+
+			try
+			{
+				for (int i = 0; i < scripts.Length; i++)
+				{
+					var script = scripts[i];
+					File.Copy($"{ProjectBasePath}/{script}/bin/{script}.dll", $"{GameScriptDirectory}/Script{i}_{script}.TrainerScript", true);
+					Console.WriteLine($"Copy GTA5Trainer/Script{i}_{script}.TrainerScript Success");
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+				Console.WriteLine(e.StackTrace);
+			}
+		}
+	}
+}
---
+++ CopyTool/WeaponJson.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class Tint
+{
+	public int Index { get; set; }
+	public DisplayName TranslatedLabel { get; set; }
+}
+
+[Serializable]
+public class Components
+{
+	public string Name { get; set; }
+	public bool IsDefault { get; set; }
+
+	public DisplayName TranslatedLabel { get; set; }
+	public DisplayName TranslatedDescription { get; set; }
+	public long Hash { get; set; }
+	public int IntHash { get; set; }
+	public string AttachBone { get; set; }
+	public string Type { get; set; }
+	public string DlcName { get; set; }
+}
+
+[Serializable]
+public class Variants
+{
+	public int Index { get; set; }
+	public string Name { get; set; }
+}
+
+[Serializable]
+public class Liveries
+{
+	public string Name { get; set; }
+	public long Hash { get; set; }
+	public int IntHash { get; set; }
+	public DisplayName TranslatedLabel { get; set; }
+	public string DlcName { get; set; }
+}
+
+[Serializable]
+public class Weapon
+{
+	public string Name { get; set; }
+	public DisplayName TranslatedLabel { get; set; }
+	public long Hash { get; set; }
+	public int IntHash { get; set; }
+	public string DlcName { get; set; }
+	public string Category { get; set; }
+	public string ModelName { get; set; }
+	public string AmmoType { get; set; }
+	public string AmmoModelName { get; set; }
+	public int DefaultMaxAmmoSp { get; set; }
+	public int SkillAbove50MaxAmmoSp { get; set; }
+	public int MaxSkillMaxAmmoSp { get; set; }
+	public int DefaultMaxAmmoMp { get; set; }
+	public int SkillAbove50MaxAmmoMp { get; set; }
+	public int MaxSkillMaxAmmoMp { get; set; }
+	public int BonusMaxAmmoMp { get; set; }
+	public string DamageType { get; set; }
+	public DisplayName TranslatedDescription { get; set; }
+	public List<Tint> Tints { get; set; }
+	public bool IsVehicleWeapon { get; set; }
+	public List<string> Flags { get; set; }
+	public List<Components> Components { get; set; }
+	public List<Liveries> Liveries { get; set; }
+}
