7767274d1eee778ac3cf7f04a8d0d8c40a020bdc
Da-StackOverflow <739050313@qq.com>
2025-03-04 16:30:20 +0800

    Update Some Scripts

--- Vehicle/GetInCar.cs
+++ Vehicle/GetInCar.cs
@@ -15,16 +15,19 @@ namespace Vehicle
 			GET_ENTITY_PLAYER_IS_FREE_AIMING_AT(PlayerID, &entity);
 			if (IS_ENTITY_A_VEHICLE(entity))
 			{
-				for(int i = -1; i <= 4; ++i)
+				var seats = GET_VEHICLE_MODEL_NUMBER_OF_SEATS(GET_ENTITY_MODEL(entity));
+				for (int i = 0; i < seats; ++i)
 				{
-					if(IS_VEHICLE_SEAT_FREE(entity, i, true))
+					if(IS_VEHICLE_SEAT_FREE(entity, i - 1, true))
 					{
-						SET_PED_INTO_VEHICLE(PlayerPed, entity, i);
+						SET_PED_INTO_VEHICLE(PlayerPed, entity, i - 1);
 						return;
 					}
 				}
-				
+				SetTips("没有空座位");
+				return;
 			}
+			SetTips("没有瞄准到汽车");
 		}
 	}
 }
